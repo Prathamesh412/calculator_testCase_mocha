a6364654bc2e112e1bc69a6c5fa1ddc6211a16e5
Prathamesh <prathprabhu@gmail.com>
2015-12-03 10:04:35 +0530

    First

---
+++ app.js
@@ -0,0 +1,10 @@
+var calculator = require('./app/calculator');
+
+var addResult = calculator.add(2,3);
+console.log(addResult);
+var multiplyResult = calculator.multiply(2,3);
+console.log(multiplyResult);
+var divisionResult = calculator.division(6,3);
+console.log(divisionResult);
+var substractResult = calculator.substract(9,3);
+console.log(substractResult);
---
+++ app/calculator.js
@@ -0,0 +1,22 @@
+
+
+exports.add = function add(a,b)
+{
+  return a+b;
+}
+
+exports.multiply=function multiply(a,b)
+{
+  return a*b;
+}
+
+exports.division=function division(a,b)
+{
+  return a/b;
+}
+
+exports.substract=function substract(a,b)
+{
+  return a-b;
+}
+
